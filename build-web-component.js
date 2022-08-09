6a2d6e6d8278fac0a9f4c471f3779834dc48fd2f
Marc Pons Serrano <marc.pons.serrano@everis.com>
2022-08-09 16:11:24 +0200

    chore: update build element script

--- build-web-component.js
+++ build-web-component.js
@@ -2,9 +2,9 @@ const fs = require("fs-extra");
 const concat = require("concat");
 build = async () => {
   const files = [
-    "./dist/angular-web-component/runtime.js",
-    "./dist/angular-web-component/polyfills.js",
-    "./dist/angular-web-component/main.js",
+    "./dist/angular-web-component-v9/runtime-es2015.js",
+    "./dist/angular-web-component-v9/polyfills-es2015.js",
+    "./dist/angular-web-component-v9/main-es2015.js",
   ];
 
   await fs.ensureDir("src/assets");
